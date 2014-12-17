54ce0fe9fabe15a63a737c2878c96339f6ae409e
jake-white <acorns1225>
2014-12-17 16:19:56 -0500

    oops

--- js/DrawingScreen.js
+++ js/DrawingScreen.js
@@ -1,16 +1,22 @@
-var canvas;
+var canvas, FOV = 1;
 var DrawingScreen = function(c){
     canvas = c;
 }
 
 DrawingScreen.prototype.drawShit = function(cD){
+
     var ctx = canvas.getContext("2d");
-    console.log(cD.length);
-    for(var x = 0; x < cD.length; ++x)
-    {
+    for(var x = 0; x < cD.length; ++x) {
+        var intensity = 3 / cD[x];
+        if(intensity>1)
+        intensity=1;
+        ctx.strokeStyle = "rgb(" + 255*intensity+","+ 255 * intensity+","+ 255* intensity+")";
+        if (cD[x] > 0) {
+        var height = ((1 / cD[x]) * ((canvas.width / 2) / (FOV / 2)));
         ctx.beginPath();
-        ctx.moveTo(0,0);
-        ctx.lineTo(cD[x])
+        ctx.moveTo(x, 300 - (height / 2));
+        ctx.lineTo(x, 300 + (height / 2));
         ctx.stroke();
+    }
     }
 }
