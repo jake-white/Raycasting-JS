cc3f46de4eda2d883577dbe6e471a7ff584d156b
jake-white <acorns1225>
2014-12-18 20:07:38 -0500

    changed color scheme, added centerline

--- js/DrawingScreen.js
+++ js/DrawingScreen.js
@@ -7,15 +7,20 @@ DrawingScreen.prototype.drawShit = function(cD){
 
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.moveTo(0,canvas.height/2);
+	ctx.strokeStyle = "black"
+	ctx.lineTo(canvas.width, canvas.height/2);
+	ctx.stroke();
     for(var x = 0; x < cD.length; ++x) {
         if (cD[x] != -1) {
         var height = ((1 / cD[x]) * ((canvas.width / 2) / (FOV / 2)));
         var intensity = 3/cD[x];
-      //      console.log(intensity + " " + height + " " + x);
+      //  console.log(intensity + " " + height + " " + x);
         if(intensity>1)
             intensity=1;
         ctx.beginPath();
-   //     ctx.strokeStyle = "rgb("+255*intensity+","+255*intensity+","+255*intensity+")";
+		ctx.strokeStyle = "rgb(232, 20, 50)";
+     //   ctx.strokeStyle = "rgb("+255*intensity+","+255*intensity+","+255*intensity+")";
         ctx.moveTo(x, 300 - (height / 2));
         ctx.lineTo(x, 300 + (height / 2));
         ctx.stroke();
